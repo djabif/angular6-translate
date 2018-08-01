657c988bf26d6d21df74034761b82a8bd1241330
Gonzalo Di Giovanni <gonza.digiovanni@gmail.com>
2018-08-01 15:59:18 -0300

    Without multi-languages

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, LOCALE_ID, Inject } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  languages = [
+    { code: 'en', label: 'English'},
+    { code: 'es', label: 'Español'}
+  ];
+
+}
